563dc76861e99f0142a9632cee12ab2c53cb16e5
Samat K Jain <samat@samat.org>
2014-02-17 20:03:53 -0700

    Use stylus for stylesheets

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.enable('trust proxy');
 
